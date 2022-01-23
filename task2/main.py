d40d347211ffda278dc2b0034da2592ae72a3ffd
Andrew <elbroandrew@gmail.com>
2022-01-23 20:10:57 +1000

    feat: add table precision for float

--- task2/main.py
+++ task2/main.py
@@ -9,9 +9,9 @@ script for getting disk stats.
 def get_disk_stats(path: str) -> dict:
     # TODO определить в Мб или Гб выдавать инфу
     total, used, free = disk_usage(path)
-    total = '{:.2f}'.format(total / 1024 / 1024 / 1024)
-    used = '{:.2f}'.format(used / 1024 / 1024 / 1024)
-    free = '{:.2f}'.format(free / 1024 / 1024 / 1024)
+    total = '{:.1f}'.format(total / 1024 / 1024 / 1024)
+    used = '{:.1f}'.format(used / 1024 / 1024 / 1024)
+    free = '{:.1f}'.format(free / 1024 / 1024 / 1024)
 
     return dict(total=total, used=used, free=free)
 
@@ -20,6 +20,7 @@ def draw_stats_table(dic: dict):
     table = Texttable()
     table.set_cols_align(["c", "c", "c"])
     table.set_cols_valign(["m", "m", "m"])
+    table.set_precision(1)
     table.add_rows([["Total", "Used", "Free"],
                     [dic["total"],
                      dic["used"],
