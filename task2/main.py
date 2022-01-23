07c2932de7088d7246a327e16382b6924981c0fd
Andrew <elbroandrew@gmail.com>
2022-01-23 20:08:01 +1000

    feat: draw table

--- task2/main.py
+++ task2/main.py
@@ -1,22 +1,36 @@
 from shutil import disk_usage
 from texttable import Texttable
 
+"""
+script for getting disk stats.
+"""
+
 
 def get_disk_stats(path: str) -> dict:
-
     # TODO определить в Мб или Гб выдавать инфу
     total, used, free = disk_usage(path)
-    print('{:.2f}'.format(total / 1024 / 1024 / 1024))
-    print(used / 1024 / 1024 / 1024)
-    print(free / 1024 / 1024 / 1024)
+    total = '{:.2f}'.format(total / 1024 / 1024 / 1024)
+    used = '{:.2f}'.format(used / 1024 / 1024 / 1024)
+    free = '{:.2f}'.format(free / 1024 / 1024 / 1024)
 
     return dict(total=total, used=used, free=free)
 
 
+def draw_stats_table(dic: dict):
+    table = Texttable()
+    table.set_cols_align(["c", "c", "c"])
+    table.set_cols_valign(["m", "m", "m"])
+    table.add_rows([["Total", "Used", "Free"],
+                    [dic["total"],
+                     dic["used"],
+                     dic["free"]]
+                    ])
+    print(table.draw())
 
 
 def main():
-    get_disk_stats("C:")
+    disk_info = get_disk_stats("C:")
+    draw_stats_table(disk_info)
 
 
 if __name__ == '__main__':
