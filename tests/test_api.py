37f3406c3e1fbfc2b2c7a3a8815f7575be8a8fa5
Andrew <elbroandrew@gmail.com>
2022-01-24 17:54:11 +1000

    feat: logging assertion errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,12 +2,21 @@ import requests
 import logging
 from jsonschema import validate
 
+LOGGER = logging.getLogger(__name__)
+
 
 def test_get_headers(base_url):
-    logging.info("Testing status code 200 -- send request to /headers")
+    LOGGER.info("Testing status code 200 -- send request to https://httpbin.org/headers")
     res = requests.get(f"{base_url}/headers")
-    logging.info("Done.")
-    assert 200 == res.status_code
+
+    try:
+        assert 200 == res.status_code
+    except AssertionError as e:
+        LOGGER.error(e)
+        raise AssertionError
+
+    LOGGER.info(f"assert 200 == {res.status_code} status code")
+    LOGGER.info("Done.")
 
     schema = {
         "type": "object",
